cf2708ae1575db464f9ffc83998c2674bf56d970
tw1ttt3r <tw1ttt3r@gmail.com>
2026-03-11 00:57:01 -0600

    feat: modularize supabase  client

---
+++ src/config/supabase.config.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
+
+export default sb;
